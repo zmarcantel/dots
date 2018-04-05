8575bc1f1b6986bfbd07b4ad7b8ec09bd4944597
Zach Marcantel <zmarcantel@gmail.com>
2018-04-04 20:54:17 -0500

    safety in the vim installer, and considers nvim setup

--- vim/.vim_install.sh
+++ vim/.vim_install.sh
@@ -1,17 +1,39 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+
+# create symlink for neovim init.vim
+
 VUNDLE_SRC=https://github.com/VundleVim/Vundle.vim.git
+VUNDLE_DEST=$HOME/.vim/bundle/Vundle.vim
 
 if [ "$#" -eq 0 ]; then
     echo "expected arguments to pass to YouCompleteMe installation"
     exit 1
 fi
 
-# install Vundle base
-git clone $VUNDLE_SRC $HOME/.vim/bundle/Vundle.vim
+#  link nvim to includes-only now
+if [ ! -L "$HOME/.config/nvim/init.vim" ]; then
+    mkdir -p $HOME/.config/nvim/
+    ln -s $HOME/.vim_includes $HOME/.config/nvim/init.vim
+fi
 
-# install all listed plugins
-vim +PluginInstall +qall
+
+# install Vundle base
+if [ ! -e "$VUNDLE_DEST" ]; then
+    git clone $VUNDLE_SRC $VUNDLE_DEST
+fi
+
+# install all listed plugins (NOTE: using nvim)
+nvim +PluginInstall +qall
 
 # spawn with new working dir and call YCM install
 ( cd ~/.vim/bundle/YouCompleteMe ; ./install.py $@ )
+
+# finally, transition to the full vim config
+rm $HOME/.config/nvim/init.vim
+ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
+
+pip2 install --user --upgrade neovim
+pip3 install --user --upgrade neovim
