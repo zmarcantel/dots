389eee40447e5c7ff392f684377e400d044e5cac
Zach Marcantel <zmarcantel@gmail.com>
2018-06-04 17:06:48 -0500

    invert os_check booleans -- defaulted to linux so likely just unseen

--- env/.zmenv/os_check.sh
+++ env/.zmenv/os_check.sh
@@ -2,23 +2,23 @@
 
 is_linux() {
     if [ "$(uname)" = "Linux" ]; then
-        return 1
-    else
         return 0
+    else
+        return 1
     fi
 }
 
 is_mac() {
     if [ "$(uname)" = "Darwin" ]; then
-        return 1
-    else
         return 0
+    else
+        return 1
     fi
 }
 
 is_ubuntu() {
     if ! is_linux; then
-        return 0
+        return 1
     fi
 
     return $(lsb_release -a | grep "Ubuntu")
