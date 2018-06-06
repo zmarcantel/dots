988319021bf844693a5794a83b1987d8c7c1eab6
Zach Marcantel <zmarcantel@gmail.com>
2018-06-06 16:35:54 -0500

    support non-neovim

--- vim/.vim_install.sh
+++ vim/.vim_install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
 
 # create symlink for neovim init.vim
 
@@ -13,10 +12,26 @@ if [ "$#" -eq 0 ]; then
     exit 1
 fi
 
-#  link nvim to includes-only now
-if [ ! -L "$HOME/.config/nvim/init.vim" ]; then
-    mkdir -p $HOME/.config/nvim/
-    ln -s $HOME/.vim_includes $HOME/.config/nvim/init.vim
+using_neovim() {
+    type "nvim" > /dev/null 2>&1
+}
+
+
+if using_neovim; then
+    export VIM=nvim
+    export VIM_CONFIG_DIR=$HOME/.config/nvim
+    export VIM_CONFIG=$VIM_CONFIG_DIR/init.vim
+else
+    export VIM=vim
+    export VIM_CONFIG_DIR=$HOME
+    export VIM_CONFIG=$VIM_CONFIG_DIR/.vimrc
+    mv $VIM_CONFIG $VIM_CONFIG.orig-reloc
+fi
+
+#  link vim to includes-only now
+if [ ! -L "$VIM_CONFIG" ]; then
+    mkdir -p $VIM_CONFIG_DIR
+    ln -s $HOME/.vim_includes $VIM_CONFIG
 fi
 
 
@@ -26,14 +41,17 @@ if [ ! -e "$VUNDLE_DEST" ]; then
 fi
 
 # install all listed plugins (NOTE: using nvim)
-nvim +PluginInstall +qall
+$VIM +PluginInstall +qall
 
 # spawn with new working dir and call YCM install
 ( cd ~/.vim/bundle/YouCompleteMe ; ./install.py $@ )
 
 # finally, transition to the full vim config
-rm $HOME/.config/nvim/init.vim
-ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
-
-pip2 install --user --upgrade neovim
-pip3 install --user --upgrade neovim
+rm $VIM_CONFIG
+if using_neovim; then
+    ln -s $HOME/.vimrc $VIM_CONFIG
+    pip2 install --user --upgrade neovim
+    pip3 install --user --upgrade neovim
+else
+    mv $VIM_CONFIG.orig-reloc $VIM_CONFIG
+fi
--- vim/.vimrc
+++ vim/.vimrc
@@ -15,6 +15,7 @@ set cuc
 set number
 
 set t_Co=256
+syntax on
 color zach
 
 
